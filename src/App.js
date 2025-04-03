0b9623f1b1936d3c768cac034784cdf23fab1451
igi023 <igormolnar27@gmail.com>
2025-04-03 15:30:22 +0200

    uploaded videos to home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const VIDEOS = [
+  {
+    id:'1',
+    title:'How to learn js',
+    url:'https://www.youtube.com/watch?v=PkZNo7MFNFg&ab_channel=freeCodeCamp.org',
+    cover:'https://i.ytimg.com/vi/PkZNo7MFNFg/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLAguxhrjNZKsuDntmFfRCV9gZbtEQ',
+  },
+
+  {
+    id:'2',
+    title:'How to learn php',
+    url:'https://m.youtube.com/watch?v=ZdP0KM49IVk&pp=ygUQaG93IHRvIGxlYXJuIHBocNIHCQliAAYKOfT1XA%3D%3D',
+    cover:'https://i.ytimg.com/vi_webp/ZdP0KM49IVk/hqdefault.webp',
+
+  }
+
+
+];
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
+    <>
+   
+      {VIDEOS.map(video =>
+
+       <div className='container d-flex m-5'>
+        <a key={video.id} href={video.url} target='blank'>
+        <img src={video.cover}/>
+        <p>{video.title}</p>
         </a>
-      </header>
-    </div>
+      </div>  
+
+      )}
+       
+         
+
+
+    </>
+ 
+
+
   );
 }
 
